3e2cb8f15f1db1daf54ed7647e23a9eb2b1792f2
Ammar <kurd.ammar@gmail.com>
2020-02-07 21:48:44 +0300

    Create mux2.v

---
+++ Week-1/mux2.v
@@ -0,0 +1,8 @@
+/*
+Disclaimer: I do not own this code, it was adopted from the reference book: 
+Digital Design: An Embedded Systems Approach Using Verilog by Peter J. Ashenden.
+*/
+
+module mux2 (output y, sel1, input i0, i1);
+  assign y = sel1 ? i1 : i0;
+endmodule
