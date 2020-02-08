ed72a0eb43b747579b7ea7051fe1cca0647ab83d
Ammar <kurd.ammar@gmail.com>
2020-02-08 20:09:11 +0300

    Update mux2.v

--- Week-1/mux2.v
+++ Week-1/mux2.v
@@ -3,6 +3,6 @@ Disclaimer: I do not own this code, it was adopted from the reference book:
 Digital Design: An Embedded Systems Approach Using Verilog by Peter J. Ashenden.
 */
 
-module mux2 (output y, sel1, input i0, i1);
+module mux2 (output y, input i0, i1, sel1);
   assign y = sel1 ? i1 : i0;
 endmodule
